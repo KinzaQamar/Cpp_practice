c2d27b5f71a6c88f257e14c1d7a12cca63e22c36
KinzaQamar <kinzahqamarzaman@gmail.com>
2023-11-29 01:20:43 +0500

    Header added and alongwith scope resolution operator example

--- local_and_global_variable/local_and_global_variable.cpp
+++ local_and_global_variable/local_and_global_variable.cpp
@@ -1,3 +1,6 @@
+// This program explains the difference of global and local variables
+// Edit : 29-Nov-2023
+
 #include <iostream>
 
 int glob_var = 10;
@@ -11,7 +14,8 @@ int main() {
    
     //Local and Global variable could have same names but inside main the precedence will be given to local named varible
     int glob_var = 89;
-    std::cout<<"Printing glob_var from main : "<<glob_var;
+    std::cout<<"Printing glob_var of main scope : "<<glob_var;
+    std::cout<<std::endl<<"Printing glob_var from main scope : "<<::glob_var; // '::' is scope resolution operator
 
     return 0;
 }
