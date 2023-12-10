09f4b539d0a23b0b67c7da77a704e6f78670e638
KinzaQamar <kinzahqamarzaman@gmail.com>
2023-12-10 23:50:38 +0500

    Added example of virtual functions for run-time polymorphism

---
+++ virtual_functions/virtual_funcs.cpp
@@ -0,0 +1,88 @@
+//This example explain the use of run time polymorphism as well as use of pointers with derived classes
+//10-Dec-2023
+
+#include <iostream>
+using namespace std;
+
+class Base {
+    int property_a;
+
+    public:
+        int property_b;
+
+        Base() {}
+
+        Base (int a, int b) {
+            property_a = a;
+            property_b = b;
+        }
+
+        void set_a (int a) {
+            property_a = a;
+        }
+
+        void set_b (int b) {
+            property_b = b;
+        }
+
+        virtual void print () {
+            cout<<"The value of Base class Property A : "<<property_a
+                <<" and Property B : "<<property_b<<endl;
+        }
+};
+
+class Derived : public Base {
+    int property_c;
+
+    public:
+        int property_d;
+
+        Derived () {}
+
+        Derived (int c, int d) {
+            property_c = c;
+            property_d = d;
+        }
+
+        void set_c (int c) {
+            property_c = c;
+        }
+
+        void set_d (int d) {
+            property_d = d;
+        }
+
+        void print () {
+            cout<<"Printing function of Derived class"<<endl;
+            Base :: print();
+            cout<<"The value of Derived class Property C : "<<property_c
+                <<" and Property D : "<<property_d<<endl<<endl;
+        }
+};
+
+int main() {
+    Base *b2;
+    Derived d1, *d2;
+
+    d1 = Derived(10, 20);
+    d1.set_b(90);
+    d1.print();
+
+    b2 = &d1;
+    b2->print();
+    b2->set_a(8); //same as d1.set_a(8)
+    b2->set_b(9);
+    b2->print(); //run time function -- do function binding at run time to derived :: print()
+
+    d2 = &d1;
+    d2->print();
+    d2->set_d(100);
+    b2->set_a(800);
+    b2->set_b(900);
+    d2->print();
+    d1.print();
+
+    //b2.set_c(7); //Unable to use derived class function
+
+    return 0;
+}
